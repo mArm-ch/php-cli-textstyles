a3f6de5b959475256747a4c009984aa215a03dd5
David Ansermot <david.ansermot@gmail.com>
2023-04-21 21:28:37 +0200

    Fixed codes, removed br

--- CLITextStyles.php
+++ CLITextStyles.php
@@ -49,69 +49,69 @@ final class CLITS {
  *  Font styles
  */
 final class CLITSFontStyles {
-	public const Bold = "\033".'[1m'."\n";
-	public const Dim = "\033".'[2m'."\n";
-	public const Underline = "\033".'[4m'."\n";
-	public const Blink = "\033".'[5m'."\n";
-	public const Inverted = "\033".'[7m'."\n";
+	public const Bold = "\033".'[1m';
+	public const Dim = "\033".'[2m';
+	public const Underline = "\033".'[4m';
+	public const Blink = "\033".'[5m';
+	public const Inverted = "\033".'[7m';
 }
 
 /**
  *  Styles resets
  */
 final class CLITSResets {
-	public const All = "\033".'[0m'."\n";
-	public const Bold = "\033".'[21m'."\n";
-	public const Dim = "\033".'[22m'."\n";
-	public const Underline = "\033".'[24m'."\n";
-	public const Blink = "\033".'[25m'."\n";
-	public const Inverted = "\033".'[27m'."\n";
+	public const All = "\033".'[0m';
+	public const Bold = "\033".'[21m';
+	public const Dim = "\033".'[22m';
+	public const Underline = "\033".'[24m';
+	public const Blink = "\033".'[25m';
+	public const Inverted = "\033".'[27m';
 }
 
 /**
  *  Foreground colors
  */
 final class CLITS16ForeColors {
-	public const System = "\033".'[39m'."\n";
-	public const Black = "\033".'[30m'."\n";
-	public const Red = "\033".'[31m'."\n";
-	public const Green = "\033".'[32m'."\n";
-	public const Yellow = "\033".'[33m'."\n";
-	public const Blue = "\033".'[34m'."\n";
-	public const Magenta = "\033".'[35m'."\n";
-	public const Cyan = "\033".'[36m'."\n";
-	public const LightGray = "\033".'[37m'."\n";
-	public const DarkGray = "\033".'[90m'."\n";
-	public const LightRed = "\033".'[91m'."\n";
-	public const LightGreen = "\033".'[92m'."\n";
-	public const LightYellow = "\033".'[93m'."\n";
-	public const LightBlue = "\033".'[94m'."\n";
-	public const LightMagenta = "\033".'[95m'."\n";
-	public const LightCyan = "\033".'[96m'."\n";
-	public const White = "\033".'[97m'."\n";
+	public const System = "\033".'[39m';
+	public const Black = "\033".'[30m';
+	public const Red = "\033".'[31m';
+	public const Green = "\033".'[32m';
+	public const Yellow = "\033".'[33m';
+	public const Blue = "\033".'[34m';
+	public const Magenta = "\033".'[35m';
+	public const Cyan = "\033".'[36m';
+	public const LightGray = "\033".'[37m';
+	public const DarkGray = "\033".'[90m';
+	public const LightRed = "\033".'[91m';
+	public const LightGreen = "\033".'[92m';
+	public const LightYellow = "\033".'[93m';
+	public const LightBlue = "\033".'[94m';
+	public const LightMagenta = "\033".'[95m';
+	public const LightCyan = "\033".'[96m';
+	public const White = "\033".'[97m';
 }
 
 /**
  *  Background colors
  */
 final class CLITS16BackColors {
-	public const System = "\033".'[49m'."\n";
-	public const Black = "\033".'[40m'."\n";
-	public const Red = "\033".'[41m'."\n";
-	public const Green = "\033".'[42m'."\n";
-	public const Yellow = "\033".'[43m'."\n";
-	public const Blue = "\033".'[44m'."\n";
-	public const Magenta = "\033".'[45m'."\n";
-	public const Cyan = "\033".'[46m'."\n";
-	public const LightGray = "\033".'[47m'."\n";
-	public const DarkGray = "\033".'[100m'."\n";
-	public const LightRed = "\033".'[101m'."\n";
-	public const LightGreen = "\033".'[102m'."\n";
-	public const LightYellow = "\033".'[103m'."\n";
-	public const LightBlue = "\033".'[104m'."\n";
-	public const LightMagenta = "\033".'[105m'."\n";
-	public const LightCyan = "\033".'[106m'."\n";
-	public const White = "\033".'[107m'."\n";
+	public const System = "\033".'[49m';
+	public const Black = "\033".'[40m';
+	public const Red = "\033".'[41m';
+	public const Green = "\033".'[42m';
+	public const Yellow = "\033".'[43m';
+	public const Blue = "\033".'[44m';
+	public const Magenta = "\033".'[45m';
+	public const Cyan = "\033".'[46m';
+	public const LightGray = "\033".'[47m';
+	public const DarkGray = "\033".'[100m';
+	public const LightRed = "\033".'[101m';
+	public const LightGreen = "\033".'[102m';
+	public const LightYellow = "\033".'[103m';
+	public const LightBlue = "\033".'[104m';
+	public const LightMagenta = "\033".'[105m';
+	public const LightCyan = "\033".'[106m';
+	public const White = "\033".'[107m';
 }
 
 ?>
